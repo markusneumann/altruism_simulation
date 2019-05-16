1db46fcfad3ffe337e17832869f90303c5a11d9c
markusneumann <mneumann.polsci@gmail.com>
2019-05-16 11:12:43 -0400

    png version of contagion img

--- code/illustration_Contagion.R
+++ code/illustration_Contagion.R
@@ -11,14 +11,14 @@ generations <- 100
 
 #Run the simulation
 source("Neumann_Altruism_Simulation_IGRAPH_dont_reset_img.R")
-
+save.image("../results/img_contagion_illustration.rdata")
 nodeOfInterest <- 200
 colfunc <- colorRampPalette(c("green", "red"))
 colNodes <- colfunc(12)[img[,nodeOfInterest]+5]
 colNodes[img[,nodeOfInterest]==0] <- "#808080"
 colNodes[nodeOfInterest] <- "#0000ff"
 pdf(file = "../figures/illustration_Contagion.pdf", width = 12, height = 12)
-png(file = "../figures/illustration_Contagion.pdf", width = 800, height = 800)
+png(file = "../figures/illustration_Contagion.png", width = 800, height = 800)
 set.seed(1)
 plot(g, vertex.size=6, vertex.label=NA, vertex.color=colNodes) #alternatively, vertex.size=(degree(g))*0.6
 dev.off()
